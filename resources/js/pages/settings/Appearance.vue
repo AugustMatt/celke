6dec66cc80adc13f50c72e18519c12e7a24ec077
Matheus Amaral <matheusaugusto@hospitalriogrande.com.br>
2025-11-25 16:07:11 -0300

    Tradução das paginas

---
+++ resources/js/pages/settings/Appearance.vue
@@ -0,0 +1,34 @@
+<script setup lang="ts">
+import { Head } from '@inertiajs/vue3';
+
+import AppearanceTabs from '@/components/AppearanceTabs.vue';
+import HeadingSmall from '@/components/HeadingSmall.vue';
+import { type BreadcrumbItem } from '@/types';
+
+import AppLayout from '@/layouts/AppLayout.vue';
+import SettingsLayout from '@/layouts/settings/Layout.vue';
+import { edit } from '@/routes/appearance';
+
+const breadcrumbItems: BreadcrumbItem[] = [
+    {
+        title: 'Gerenciar Aparência',
+        href: edit().url,
+    },
+];
+</script>
+
+<template>
+    <AppLayout :breadcrumbs="breadcrumbItems">
+        <Head title="Gerenciar Aparência" />
+
+        <SettingsLayout>
+            <div class="space-y-6">
+                <HeadingSmall
+                    title="Aparência"
+                    description="Personalize a aparência do aplicativo conforme sua preferência."
+                />
+                <AppearanceTabs />
+            </div>
+        </SettingsLayout>
+    </AppLayout>
+</template>
